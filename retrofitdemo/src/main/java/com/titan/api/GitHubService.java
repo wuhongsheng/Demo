cef0bd6f839eed25a354186b72146d5786f95e8c
WHS <664116298@qq.com>
2016-12-07 16:56:41 +0800

    增加Retrofit DEMO

--- retrofitdemo/src/main/java/com/titan/api/GitHubService.java
+++ retrofitdemo/src/main/java/com/titan/api/GitHubService.java
@@ -6,6 +6,7 @@ import com.titan.model.Contributor;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.Callback;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
@@ -14,4 +15,9 @@ public interface GitHubService {
     Call<List<Contributor>> contributors(
             @Path("owner") String owner,
             @Path("repo") String repo);
+    @GET("/repos/{owner}/{repo}/contributors")
+    Call<List<Contributor>> contributors(
+            @Path("owner") String owner,
+            @Path("repo") String repo, Callback<List<Contributor>> response);
+
 }
--- retrofitdemo/src/main/java/com/titan/retrofitdemo/MainActivity.java
+++ retrofitdemo/src/main/java/com/titan/retrofitdemo/MainActivity.java
@@ -2,6 +2,8 @@ package com.titan.retrofitdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.titan.api.GitHubService;
 import com.titan.model.Contributor;
@@ -15,32 +17,52 @@ import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
     public static final String API_URL = "https://api.github.com";
+    TextView tv;
+    String info = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Create a very simple REST adapter which points the GitHub API.
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(API_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        // Create an instance of our GitHub API interface.
-        GitHubService github = retrofit.create(GitHubService.class);
-
-        // Create a call instance for looking up Retrofit contributors.
-        Call<List<Contributor>> call = github.contributors("square", "retrofit");
-
-        // Fetch and print a list of the contributors to the library.
-        List<Contributor> contributors = null;
-        try {
-            contributors = call.execute().body();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        for (Contributor contributor : contributors) {
-            System.out.println(contributor.login + " (" + contributor.contributions + ")");
-        }
+        tv = (TextView) findViewById(R.id.info);
     }
 
+    public void onGetInfo(View view) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // Create a very simple REST adapter which points the GitHub API.
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl(API_URL)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+                // Create an instance of our GitHub API interface.
+                GitHubService github = retrofit.create(GitHubService.class);
+
+                // Create a call instance for looking up Retrofit contributors.
+                Call<List<Contributor>> call = github.contributors("square", "retrofit");
+                // Fetch and print a list of the contributors to the library.
+                List<Contributor> contributors = null;
+
+                try {
+                    contributors = call.execute().body();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                for (Contributor contributor : contributors) {
+                    info = info + "\n" + contributor.login + " (" + contributor.contributions + ")";
+                    System.out.println(contributor.login + " (" + contributor.contributions + ")");
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tv.setText(info);
+                    }
+                });
+
+            }
+        }).start();
+
+    }
 }
